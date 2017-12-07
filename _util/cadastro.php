24cd091112c588af73ea7670e647c1a755aaf3d3
Alyson Dantas <alyson.lipe@gmail.com>
2017-12-07 17:41:39 -0300

    Só para organizar o script de cadastro

--- _util/cadastro.php
+++ _util/cadastro.php
@@ -1,35 +1,35 @@
 <?php
 
 //$bd = pg_connect("host=localhost port=5432 dbname=pbl user=lucas password=lucas") or die ("Não foi possível conectar ao servidor PostGreSQL");
-require_once('userdao.php');
-require_once('../_model/Usuario.php');
+require_once( 'userdao.php' );
+require_once( '../_model/Usuario.php' );
 
-if ((isset($_POST['nome']) == false) and (isset($_POST['email']) == false) and (isset($_POST['senha']) == false) and (isset($_POST['password']) == false)) {
+if ( ( isset( $_POST[ 'nome' ] ) == false )and( isset( $_POST[ 'email' ] ) == false )and( isset( $_POST[ 'senha' ] ) == false )and( isset( $_POST[ 'password' ] ) == false ) ) {
 
-    //header("Location:index.php");
-    echo 'deu ruim';
-}else{
+	//header("Location:index.php");
+	echo 'deu ruim';
+} else {
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-$confirmSenha = $_POST['confirmSenha'];
+	$nome = $_POST[ 'nome' ];
+	$email = $_POST[ 'email' ];
+	$senha = $_POST[ 'senha' ];
+	$confirmSenha = $_POST[ 'confirmSenha' ];
 
-if ($senha != $confirmSenha) {
-    echo "Saia daqui";
-}else{
+	if ( $senha != $confirmSenha ) {
+		echo "Saia daqui";
+	} else {
 
-//echo crypt($senha, '$6$rounds=5000$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$');
+		//echo crypt($senha, '$6$rounds=5000$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$');
 
-$senhaCrip = crypt($senha, '$6$rounds=5000$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$');
+		$senhaCrip = crypt( $senha, '$6$rounds=5000$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$' );
 
-$user = new Usuario($nome, '', '', $email, '', 'N', $senhaCrip);
+		$user = new Usuario( $nome, '', '', $email, '', 'N', $senhaCrip );
 
-$dao = new UserDao();
-$dao->inserir($user);
+		$dao = new UserDao();
+		$dao->inserir( $user );
 
-//header("Location:home.php");
-}
+		//header("Location:home.php");
+	}
 }
 
 ?>
