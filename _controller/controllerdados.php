c94edeb9605c158f9bcdacda441fe14d532f48d9
Alyson Dantas <alyson.lipe@gmail.com>
2017-12-08 00:09:11 -0300

    Logout funcionando

--- _controller/controllerdados.php
+++ _controller/controllerdados.php
@@ -2,63 +2,76 @@
 
 require_once( "../_util/userdao.php" );
 require_once( "../_model/Usuario.php" );
-
+//session_start();
 class Controllerdados {
 	public static $instance = null;
-	
-	private function __construct() {
-        
-    }
-	
-	public static function getInstance() {
-        if (self::$instance == NULL) {
+	private $user;
+
+	private
+	function __construct() {
+
+	}
+
+	public static
+	function getInstance() {
+		if ( self::$instance == NULL ) {
 			self::$instance = new Controllerdados();
 			echo "aqui nova instancia";
-        }
+		}
 		echo "ja tem instancia";
-        return self::$instance;
-    }
-	
-	public static function zeraSingleton(){
+		return self::$instance;
+	}
+
+	public static
+	function zeraSingleton() {
 		self::$instance = new Controllerdados();
 	}
-	
-	public function realizalogin( $email, $senha ) {
+
+	public
+	function realizalogin( $email, $senha ) {
 		$senhaCrip = crypt( $senha, '$6$rounds=5000$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$' );
 		echo " a senha é " . $senhaCrip;
 		$userdao = new UserDao();
-		$resultado = $userdao->ler($email, $senhaCrip);
+		$resultado = $userdao->ler( $email, $senhaCrip );
 
 		if ( $resultado != null ) {
 			echo "existe resultado";
 			//$resultado = pg_query($sql);
 			$linha = pg_fetch_array( $resultado );
-			
-			echo "Os dados'" . $linha[ 'idusuario' ] . " | " . $linha[ 'nome' ] . " | " . $linha['email'] . " | " . $linha[ 'senha' ]  . " | " . $linha['privilegio'] . "\n ";
-			
-			$usuario = new Usuario( $linha[ 'nome' ], '', '', $linha['email'], '', $linha['privilegio'] , $linha[ 'senha' ] );
-			$usuario->setId($linha[ 'idusuario' ]);
-			
+
+			echo "Os dados'" . $linha[ 'idusuario' ] . " | " . $linha[ 'nome' ] . " | " . $linha[ 'email' ] . " | " . $linha[ 'senha' ] . " | " . $linha[ 'privilegio' ] . "\n ";
+
+			$usuario = new Usuario( $linha[ 'nome' ], '', '', $linha[ 'email' ], '', $linha[ 'privilegio' ], $linha[ 'senha' ] );
+			$usuario->setId( $linha[ 'idusuario' ] );
+
 			$_SESSION[ 'user' ] = serialize( $usuario );
-			if ( $linha['privilegio'] == 'N' ) {
+			$this->user = unserialize($_SESSION['user']);
+			if ( $linha[ 'privilegio' ] == 'N' ) {
 				echo "usuario comum";
-				header( 'location:../painel-do-usuario.html' );
-			} else if ( $linha['privilegio'] == 'M' ) {
+				header( 'location:../painel-do-usuario.php' );
+			} else if ( $linha[ 'privilegio' ] == 'M' ) {
 				echo "usuario moderador";
 				//header( 'location:nossos-planos.html' );
-			} else if ( $linha['privilegio'] == 'A' ) {
+			} else if ( $linha[ 'privilegio' ] == 'A' ) {
 				echo "usuario adm";
 				//header( 'location:nossos-planos.html' );
 			}
-		}else{
+		} else {
 			echo "erro de senha ou email";
 		}
 		//echo "erro";
 		//header( 'location:errologin.html' );
 	}
-	
-	public function realizalogout(){
-		
+
+	public
+	function realizalogout() {
+		session_start();
+		if ( ( isset( $_SESSION[ 'user' ] ) == true ) || ( $_SESSION[ 'user' ] != "" )){
+			unset( $_SESSION[ 'user' ] );
+			header( 'location:../index.html' );
+		}
+
+		$usuario = $_SESSION[ 'user' ];
 	}
 
 }
