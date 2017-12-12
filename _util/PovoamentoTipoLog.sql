3a693309e48d1cb53e08ce856adb9626caa576ff
Alyson Dantas <alyson.lipe@gmail.com>
2017-12-12 15:53:04 -0300

    povoamento do tipo de log

---
+++ _util/PovoamentoTipoLog.sql
@@ -0,0 +1,7 @@
+--Cadastro de tipos de log
+insert into tipodelog (idacao, nome) values ('1', 'cadastro de usuário');
+insert into tipodelog (idacao, nome) values ('2', 'promoção de usuário');
+insert into tipodelog (idacao, nome) values ('3', 'exclusão de usuário');
+insert into tipodelog (idacao, nome) values ('4', 'banimento de usuário');
+insert into tipodelog (idacao, nome) values ('5', 'submissão de questão');
+insert into tipodelog (idacao, nome) values ('6', 'realização de simulado');
