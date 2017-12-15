9c66d3ad8ac93b1b2bdde76410c7e20be0a561a5
ecomp1997 <emanuel.santana1514@gmail.com>
2017-12-15 09:24:46 -0300

    tela de ajuda quase pronta
    tela quase pronta, falta umas besteiras

--- tela-de-ajuda.php
+++ tela-de-ajuda.php
@@ -28,15 +28,19 @@
 
 <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 col-lg-3 col-sm-12">
+            <div class="col-md-4 col-lg-4 col-sm-12">
 
                 <div class="col-md-10 col-lg-10 col-sm-12">
                     <h3>Dúvidas Frequentes</h3>
                     <div id="list-example" class="list-group">
-                        <a class="list-group-item list-group-item-action" href="#list-item-1">Como fazer um simulado?</a>
-                        <a class="list-group-item list-group-item-action" href="#list-item-2">Como escolher o ano simulado?</a>
-                        <a class="list-group-item list-group-item-action" href="#list-item-3">Como funciona a pontuação?</a>
-                        <a class="list-group-item list-group-item-action" href="#list-item-4">Como o que acontece ao desistir da prova?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-1">Como fazer um simulado?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-2">Como escolher o ano simulado?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-3">Como funciona a pontuaçãodo sistema?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-4">O que acontece ao desistir de uma prova?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-5">Como posso submeter uma questão para o sistema?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-6">Posso trocar de plano?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-7">Como realizar denúncias?</a>
+                        <a class="list-group-item list-group-item-action" href="#item-8">O que não devo fazer, pois pode provocar um banimento</a>
                     </div>
                    
                 </div>
@@ -44,7 +48,7 @@
             <div class="col-md-6 col-lg-6 col-sm-12">
              <div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example">
 
-                        <h4 id="list-item-1">Como fazer um simulado?</h4>
+                        <h4 id="item-1">Como fazer um simulado?</h4>
                         <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
@@ -56,12 +60,9 @@
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+                        </p>
 
-                        <h4 id="list-item-2">Como escolher o ano simulado?</h4>
+                        <h4 id="item-2">Como escolher o ano simulado?</h4>
 
                         <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
@@ -70,34 +71,9 @@
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+                      </p>
 
-                        <h4 id="list-item-3">Como funciona a minha pontuação no sistema?</h4>
-
-                        <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
-                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
-
-                        <h4 id="list-item-4">O que acontece ao desistir da prova?</h4>
+                        <h4 id="item-3">Como funciona a minha pontuação no sistema?</h4>
 
                         <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
@@ -110,13 +86,72 @@
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        </p>
+
+                        <h4 id="item-4">O que acontece ao desistir da prova?</h4>
+
+                        <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
                         gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+                     
+
+                        <h4 id="item-5">Como posso submeter uma questão para o sistema?</h4>
+
+                        <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+
+                        <h4 id="item-6">Posso trocar de plano?</h4>
+
+                        <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+
+                        <h4 id="item-7">Como realizar denúncias?</h4>
+
+                        <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+
+                        <h4 id="item-8">O que não devo fazer, pois pode provocar um banimento</h4>
+
+                        <p>gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn<br>
+                        gtnwegsfugdfhgjfbgdfgjkdjkvnxjknvjknxcjknvjkn</p>
+
                     </div>
             </div>
-            <div class="col-md-3 col-lg-3 col-sm-12">
+            <div class="col-md-2 col-lg-2 col-sm-12">
 
             </div>
 
@@ -129,7 +164,7 @@
 
             </div>
             <div class="col-md-3 col-lg-3 col-sm-12">
-                <button type="button" class="btn btn-outline-secondary">Submeter</button>
+               
             </div>
 
         </div>
