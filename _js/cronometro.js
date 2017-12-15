f3cd40ff26c340d11e49ff94159c6df8ec3bf0f7
Lucas Cardoso <thelucascardoso@gmail.com>
2017-12-15 01:52:39 -0300

    Ajuste cronometro

--- _js/cronometro.js
+++ _js/cronometro.js
@@ -3,7 +3,19 @@ var segundos = 0;
 var minutos = 0;
 var horas = 0;
 function inicio() {
-    control = setInterval(cronometro, 1000);
+
+	if (document.cookie =! null) {
+		var x = document.cookie;
+		var quebra1 = x.split(";");
+		var quebra2 = quebra1[1].split("=");
+		var quebra3 = quebra2[1].split(":");
+		horas = parseInt(quebra3[0]);
+		minutos = parseInt(quebra3[1]);
+		segundos = parseInt(quebra3[2]);
+		control = setInterval(cronometro, 1000);
+	}else{
+	control = setInterval(cronometro, 1000);
+}
 }
 
 
@@ -22,8 +34,13 @@ function cronometro() {
     if(minutos === 59){
         minutos = 0;
         horas++;
-    }
-}
+	}
+
+	var cont = horas+":"+minutos+":"+segundos;
+
+	document.cookie = "conta="+cont;
+    
+} 
 
 function ajustarInput(str) {
     var resul;
