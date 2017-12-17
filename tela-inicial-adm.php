e2ae727657a3ca3785d6cfdc688aa122dfca2645
ecomp1997 <emanuel.santana1514@gmail.com>
2017-12-17 11:22:28 -0300

    alteração dos arquivos

--- tela-inicial-adm.php
+++ tela-inicial-adm.php
@@ -47,10 +47,10 @@
 
 					<div id="item1" class="column page-heading">
 						<div class="large-card">
-							<h1>Hey there!</h1>
-							<p class="text-large">This is a big 'ole hero unit, you can use this for calling extra attention to featured content or information.</p>
-							<p>You could also add a call to action button. <em>(it's basically a jumbotron)</em></p>
-							<a class="button">Call to action</a>
+							<h1>Olá administrador!</h1>
+							<p class="text-large">Nesta página você pode ter acesso aos gráficos e informações 
+								referentes ao uso do sistema.</p>
+						
 						</div>
 					</div>
 					
