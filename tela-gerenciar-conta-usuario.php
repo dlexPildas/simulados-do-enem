6fa660d0efb5851b46e68eeef9c39d0ecfca37f6
ecomp1997 <emanuel.santana1514@gmail.com>
2017-12-19 16:47:12 -0300

    alteração das telas de cadastrar questão e outras

--- tela-gerenciar-conta-usuario.php
+++ tela-gerenciar-conta-usuario.php
@@ -10,15 +10,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gerenciar conta usuario</title>
     <link rel="stylesheet" type="text/css" href="_css/gerencia-conta.css">
-    <link rel="stylesheet" type="text/css" href="_css/bootstrap3.css">
-    <link rel="stylesheet" type="javascript/js" href="_js/bootstrap.js">
-    <link rel="stylesheet" href="_css/font-awesome.min.css">
-
-    <!-- CSS Reset -->
-    <link rel="stylesheet" href="_css/normalize.css">
-
-    <!-- Milligram CSS minified -->
-    <link rel="stylesheet" href="_css/milligram.min.css">
+    
+			
+	<!-- Milligram CSS minified -->
+	<link rel="stylesheet" href="_css/milligram.min.css">
+	
+	<!-- Main Styles -->
+    <link rel="stylesheet" href="_css/tela-inicial-adm.css">		
+	<link rel="stylesheet" type="text/css" href="_css/bootstrap3.css">	
 
 </head>
 
@@ -41,40 +40,34 @@
                         <li>
                             <a href="#charts"> Gráficos</a>
                         </li>
-                        <li>
-                            <a href="#widgets"> Widgets</a>
-                        </li>
-                        <li>
-                            <a href="#forms"> Forms</a>
-                        </li>
-                        <li>
-                            <a href="#alerts"> Alerts</a>
-                        </li>
-                        <li>
-                            <a href="#buttons">Buttons</a>
-                        </li>
-                        <li>
-                            <a href="#tables"> Tables</a>
-                        </li>
-                        <li>
-                            <a href="#grid"> Grid</a>
-                        </li>
+                     
                     </ul>
                 </div>
             </div>
 
             <div class="col-sm-5 col-md-5 col-lg-5">
-                <form>
+                <form method="POST" action="_controller/buscar_usuarios.php">
                     <div class="form-group">
-                        <label for="exampleFormControlFile1">Escolha uma prova para submeter</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                        
+                        
+                        <div class="col-sm-9 col-md-9 col-lg-9">                
+                        <input type="text"required placeholder="Digite o nome do usuário desejado"</label>
+                        </div>
+                       
+                        <div class="col-sm-3 col-md-3 col-lg-3">                
+                
+                            <div class="col-sm-6 col-md-6 col-lg-6">                
+                                <button type="submit">Procurar Usuário</button>
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">                
+                                
+                            </div>
+                        </div>
                     </div>
                 </form>
-                <input type="number" name="points" min="1995" max="2017" step="1" value="2017">
+                
             </div>
-            <div class="col-sm-4 col-md-4 col-lg-4">
-                <button type="submit">Cancelar</button>
-                <button type="submit">Submeter</button>
+            <div class="col-sm-4 col-md-4 col-lg-4">                
                 
             </div>
 
