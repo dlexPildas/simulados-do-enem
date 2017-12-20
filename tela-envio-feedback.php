deef65784e0ed9e3d925443b1e84b609bde1e4ef
ecomp1997 <emanuel.santana1514@gmail.com>
2017-12-20 10:29:27 -0300

    adição da imagem novamente

---
+++ tela-envio-feedback.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Enviar Feedback</title>
+</head>
+<body>
+        <div class="navbar">
+        	<?php include('navbar-usuario.php') ?>
+	    </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-lg-4 col-sm-12">
+                    
+                </div>
+            </div>
+        </div>
+</body>
+</html>
