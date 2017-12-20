f2303d99d6720e7a1b9ef076b13ab87adb942d75
ecomp1997 <emanuel.santana1514@gmail.com>
2017-12-20 19:19:01 -0300

    alteração de cor do background e formatação do envio

--- tela-envio-feedback.php
+++ tela-envio-feedback.php
@@ -12,15 +12,16 @@
     <link rel="stylesheet" type="text/css" href="_css/milligram.min.css">
     <link rel="stylesheet" type="text/css" href="_css/tela-inicial-adm.css">
 </head>
-<body>
+<body style="background-color:#606c76">
         <div class="navbar">
         	<?php include('navbar-usuario.php') ?>
         </div>
-        <br><br><br><br><br>
+        <br><br>
         <div class="container-fluid">
             <div class="row">                               
 					<div id="coluna1" class="col-md-2 col-sm-2 col-lg-2"></div>
-					<div id="coluna2" class="col-md-4 col-sm-4 col-lg-4">
+					<div id="coluna2" class="col-md-4 col-sm-4 col-lg-4"  style="background-color:#FFFFFF">
+                    <br><br><br>
 						<p>Envie-nos um feedback sobre a plataforma.</p>
 						<p>
 							<span></span>Feira de Santana, BR</p>
@@ -28,7 +29,8 @@
 							<span></span>Telefone: +55 75 10101010101</p>
 						
 					</div>
-					<div id="coluna3" class="col-md-4 col-sm-5 col-lg4">
+					<div id="coluna3" class="col-md-4 col-sm-5 col-lg4"  style="background-color:#FFFFFF">
+                    <br><br><br>
 						<form action="_controller/enviofeedback.php" method="POST">
                             <div id="teste" class="row">
                                 <div class="col-sm-6 form-group">
