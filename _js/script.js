c6a1c664d5eafcef23a0e032a821e7121dbb4dd4
ecomp1997 <emanuel.santana1514@gmail.com>
2017-12-21 09:14:10 -0300

    alterações de estilo e padronização

--- _js/script.js
+++ _js/script.js
@@ -1,15 +1,16 @@
-function mostraOculta(ID) {
+function mostraOculta(ID, ID2) {
 	if (document.getElementById(ID).style.display == "block") {
 		mostra(ID);		
+		esconde(ID2);
 	}
 	else {
 		esconde(ID);
 	}
 
 }
-function mostra(ID) {
-	document.getElementById(ID).style.display = "none";
+function mostra(TD) {
+	document.getElementById(TD).style.display = "none";
 }
-function esconde(ID) {
-	document.getElementById(ID).style.display = "block";
+function esconde(TD) {
+	document.getElementById(TD).style.display = "block";
 }
