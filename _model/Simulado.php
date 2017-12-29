9a78f39d4eb3b50aa45d74e7ce81e02f2451a8aa
Marcos Vinicius <marcosviniciuscl@outlook.com>
2017-12-29 00:11:18 -0200

    Implementação da classe simulado

--- _model/Simulado.php
+++ _model/Simulado.php
@@ -1,6 +1,27 @@
 <?php
-public class Simulado {
 
+ class Simulado {
+    private $idsimulado;
+    private $idusuario;
+    private $data_simulado;
+    private $tempo;
+    private $pontuacao;
+
+    public function getIdSimulado(){
+        return $this->idsimulado;
+    }
+    public function getIdUsuario(){
+        return $this->idusuario;
+    }
+    public function getDataSimulado(){
+        return $this->data_simulado;
+    }
+    public function getTempo(){
+        return $this->tempo;
+    }
+    public function getPontuacao(){
+        return $this->pontuacao;
+    }
 }
 
 ?>
