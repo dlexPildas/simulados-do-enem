3f600ba3f14b38d18295bfbf9db9cfc71e9416eb
ecomp1997 <emanuel.santana1514@gmail.com>
2018-01-09 10:25:50 -0300

    alteração

--- _js/script.js
+++ _js/script.js
@@ -1,16 +1,28 @@
 function mostraOculta(ID, ID2) {
-	if (document.getElementById(ID).style.display == "block") {
-		mostra(ID);		
-		esconde(ID2);
-	}
-	else {
-		esconde(ID);
-	}
+    if (document.getElementById(ID).style.display == "block" && document.getElementById(ID2).style.display == "block") {
+        mostra(ID);
+        alert("Usou o 1");
+    } else if (document.getElementById(ID).style.display == "block" && document.getElementById(ID2).style.display != "block") {
+        esconde(ID2);
+        mostra(ID);
+        alert("Usou o 2");
+    } else if (document.getElementById(ID).style.display != "block" && document.getElementById(ID2).style.display == "block") {
+        mostra(ID2);
+        esconde(ID);
+        alert("Usou o 3");
+    } else if (document.getElementById(ID).style.display != "block") {
+        esconde(ID);
+        alert("Usou o 4");
+    }
+
 
 }
+
 function mostra(TD) {
-	document.getElementById(TD).style.display = "none";
+    document.getElementById(TD).style.display = "none";
+    alert('teste alert');
 }
+
 function esconde(TD) {
-	document.getElementById(TD).style.display = "block";
+    document.getElementById(TD).style.display = "block";
 }
