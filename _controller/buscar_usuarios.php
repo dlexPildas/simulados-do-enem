1092e08dd2980806e5fd65339e7d2fd441aee35c
SaitamaXD <30640347+SaitamaXD@users.noreply.github.com>
2018-01-18 10:58:06 -0300

    Adicionando barra de ADM

--- _controller/buscar_usuarios.php
+++ _controller/buscar_usuarios.php
@@ -5,35 +5,47 @@
  * Date: 16/12/2017
  * Time: 11:28
  */
+
     require_once ("Controllerdados.php");
     $nome = $_POST['nome'];
-
+    $id = $_SESSION['privilegio'];
     $controller = Controllerdados::getInstance();
     $result = $controller->buscarUsuarios($nome);
     $tamanho = count($result);
-    echo $tamanho;
+    //echo $tamanho;
     if($result==false){
         echo "Erro, usuários com o nome especificado não foram encontrados.";
-        return false;
     }else{
-        //Deu certo
+        //echo " ".$id;
     }
 ?>
-<meta>
-    <table>
-        <tbody>
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link type= "text/css" rel=stylesheet href="../_css/bootstrap3.css">
+    <?php include ('../_view/navbar-adm.php')?>
+
+</head>
+<body>
+<table>
+    <tbody>
 
     <?php
-        $i = 0;
-        while ($i<$tamanho) { ?>
-            <tr>
-                <td><?php echo "Nome: ".$result[$i]->getNome(); ?> &nbsp;</td>
-                <td><?php echo"Email: ".$result[$i]->getEmail(); ?> &nbsp;</td>
-                <td><?php echo "Privilegio: ".$result[$i]->getPrivilegio(); ?> &nbsp;</td>
-                <td></td>
-            </tr>
-    <?php $i++;
+    $i = 0;
+    while ($i<$tamanho) { ?>
+        <tr>
+            <td><?php echo "Nome: ".$result[$i]->getNome(); ?> &nbsp;</td>
+            <td><?php echo "Email: ".$result[$i]->getEmail(); ?> &nbsp;</td>
+            <td><?php echo "Privilegio: ".$result[$i]->getPrivilegio(); ?> &nbsp;</td>
+            <td></td>
+        </tr>
+        <?php $i++;
     }
     ?>
-        <tbody>
-    </table>
+    <tbody>
+</table>
+
+</body>
+</html>
